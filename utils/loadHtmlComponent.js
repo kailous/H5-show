7c9f1aa6ef1647ab9d1a75cb069422ae06b47234
kailous <kailous@live.cn>
2024-01-02 14:00:25 +0800

    删除无用代码

--- utils/loadHtmlComponent.js
+++
@@ -1,9 +0,0 @@
-// loadHtmlComponent.js
-const fs = require('fs');
-const path = require('path');
-
-function loadHtmlComponent(filePath) {
-  return fs.readFileSync(path.join(__dirname, filePath), 'utf8');
-}
-
-module.exports = loadHtmlComponent;
